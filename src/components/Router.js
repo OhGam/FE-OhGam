213278d00cb6889bac38b3aec05e0265bc27cb16
Haejeong <skhj5822@naver.com>
2023-07-04 16:20:35 +0900

    feat: nav bar 페이지별 라우터 연결

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 import Main from "../pages/Main";
+import Diary from "../pages/Diary";
+import Community from "../pages/Community";
+import Setting from "../pages/Setting";
 import Detail from "../pages/Detail";
 import Create from "../pages/Create";
 import LogIn from "../pages/LogIn";
@@ -8,14 +12,19 @@ import SignUp from "../pages/SignUp";
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="*" element={<Main />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<Main />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
